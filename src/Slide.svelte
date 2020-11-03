664c2ab075af93086336023bd6803a00360e98d0
adeas-rakit <ade.arwans@gmail.com>
2020-11-03 21:25:49 +0700

    add  aria-label

--- src/Slide.svelte
+++ src/Slide.svelte
@@ -62,10 +62,10 @@
   {#if control && glide}
     <div class="glide__arrows" >
         <slot name="control" {glide}>
-            <button on:click={() => glide.go('<')} class="glide__arrow glide__arrow--left" >
+            <button on:click={() => glide.go('<')} class="glide__arrow glide__arrow--left" aria-label="Left Arrow">
               <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"><path d="M0 12l10.975 11 2.848-2.828-6.176-6.176H24v-3.992H7.646l6.176-6.176L10.975 1 0 12z"></path></svg>
             </button>
-            <button on:click={() => glide.go('>')} class="glide__arrow glide__arrow--right">
+            <button on:click={() => glide.go('>')} class="glide__arrow glide__arrow--right" aria-label="Right Arrow">
               <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"><path d="M13.025 1l-2.847 2.828 6.176 6.176h-16.354v3.992h16.354l-6.176 6.176 2.847 2.828 10.975-11z"></path></svg>
             </button>
         </slot>
@@ -75,7 +75,7 @@
     <div class="glide__bullets">
       {#each items as item, index}
           <slot name="bullet" prop={bulletIn(index, glide)}>
-              <button class:focus={bulletIn(index, glide).isActive}
+              <button  aria-label="Bullet-{index}" class:focus={bulletIn(index, glide).isActive}
                       class="glide__bullet"
                       on:click={() => bulletIn(index, glide).focus()}
               >
